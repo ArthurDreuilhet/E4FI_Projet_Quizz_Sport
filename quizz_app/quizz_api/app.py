33019172283a29c48f78bc97cec02117f0646f10
Genesi0s <pretet.cedric@gmail.com>
2025-06-04 15:00:42 +0200

    first postman test

--- quizz_app/quizz_api/app.py
+++ quizz_app/quizz_api/app.py
@@ -7,8 +7,12 @@ CORS(app)
 @app.route('/')
 def hello_world():
     x = 'world'
-    y = 'Why does Valentin say caca all the time?'
+    y = 'Why does Valentin say caca all the time?   Because he is a child at heart!'
     return f"Hello, {x}. {y}"
+
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
 
 if __name__ == "__main__":
     app.run()
