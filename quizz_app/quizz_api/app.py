54a3ef21c425f082a314e93cb28bec674c61d85b
ArthurDreuilhet <arthur.dreuilhet@edu.esiee.fr>
2025-06-04 14:57:15 +0200

    maj app.py

--- quizz_app/quizz_api/app.py
+++ quizz_app/quizz_api/app.py
@@ -1,14 +1,17 @@
 from flask import Flask
-from flask_cors import CORS
+from flask_cors import CORS	
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def hello_world():
-    x = 'world'
-    y = 'Why does Valentin say caca all the time?'
-    return f"Hello, {x}. {y}"
+	x = 'prou	t'
+	return f"Hello, {x}"
+
+@app.route('/quiz-info', methods=['GET'])
+def GetQuizInfo():
+	return {"size": 0, "scores": []}, 200
 
 if __name__ == "__main__":
     app.run()
